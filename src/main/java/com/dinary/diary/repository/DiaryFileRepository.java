73d4d230bea200948a196979fb10a51f11a1a51b
Paaarrk <lacogames87@gmail.com>
2023-11-10 23:33:59 +0900

    Initial Commit

--- src/main/java/com/dinary/diary/repository/DiaryFileRepository.java
+++
@@ -1,8 +0,0 @@
-package com.dinary.diary.repository;
-
-import com.dinary.diary.entity.DiaryFileEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DiaryFileRepository extends JpaRepository<DiaryFileEntity, Long> {
-
-}
